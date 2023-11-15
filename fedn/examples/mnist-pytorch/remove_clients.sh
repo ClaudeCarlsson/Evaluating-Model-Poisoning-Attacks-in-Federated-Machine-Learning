1364804901439b1945398aa85d50dc8035dd4a57
EdvinGermer <98961282+EdvinGermer@users.noreply.github.com>
2023-11-15 12:33:47 +0100

    added two shellscripts for MNIST

---
+++ fedn/examples/mnist-pytorch/remove_clients.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# The image name to match
+IMAGE_NAME="ghcr.io/scaleoutsystems/fedn/fedn:master-mnist-pytorch"
+
+# Find all container IDs created from the specified image
+CONTAINER_IDS=$(docker ps -q -f ancestor="$IMAGE_NAME")
+
+# Check if any containers were found
+if [ -z "$CONTAINER_IDS" ]; then
+    echo "No containers found for the image $IMAGE_NAME"
+    exit 0
+fi
+
+# Loop through each container ID and remove it
+for CONTAINER_ID in $CONTAINER_IDS
+do
+    # Stop the container
+    docker stop $CONTAINER_ID
+
+    # Remove the container
+    docker rm $CONTAINER_ID
+
+    echo "Removed container (Container ID: $CONTAINER_ID)"
+done
+
+echo "All containers created from the image $IMAGE_NAME have been removed."
---
+++ fedn/examples/mnist-pytorch/split_and_create.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# Check if the number of clients is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 NUM_CLIENTS"
+    exit 1
+fi
+NUM_CLIENTS=$1
+
+
+
+
+# Remove the /data/clients directory
+echo "Removing existing /data/clients directory..."
+rm -rf ./data/clients
+
+
+
+
+# Step 1: Split data
+echo "Splitting data into $NUM_CLIENTS parts..."
+bin/split_data --n_splits=$NUM_CLIENTS
+
+# Check if split_data was successful
+if [ $? -ne 0 ]; then
+    echo "Data splitting failed."
+    exit 1
+fi
+echo "Data split successfully."
+
+
+
+
+
+# Step 2: Run clients
+echo "Running $NUM_CLIENTS clients..."
+./run_clients.sh $NUM_CLIENTS
+
+# Check if run_clients was successful
+if [ $? -ne 0 ]; then
+    echo "Running clients failed."
+    exit 1
+fi
+echo "Clients are running."
